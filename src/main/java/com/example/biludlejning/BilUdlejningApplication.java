960bdcce8a51a86efdb32e65e1d83262d2eea3d8
shortytheman <shor7ty@gmail.com>
2022-05-03 11:45:36 +0200

    MVC setup

---
+++ src/main/java/com/example/biludlejning/BilUdlejningApplication.java
@@ -0,0 +1,13 @@
+package com.example.biludlejning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilUdlejningApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BilUdlejningApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/biludlejning/BilUdlejningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biludlejning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilUdlejningApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
