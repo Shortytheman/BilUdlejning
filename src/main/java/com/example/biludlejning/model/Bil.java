cfbea06e44d7368d363862a166694af5bb2cb3aa
shortytheman <shor7ty@gmail.com>
2022-05-05 10:01:43 +0200

    Setup complete

---
+++ src/main/java/com/example/biludlejning/model/Bil.java
@@ -0,0 +1,18 @@
+package com.example.biludlejning.model;
+
+
+public abstract class Bil {
+
+  private int vognnummer;
+  private int stelnummer;
+  private String mærke;
+  private String model;
+  private String udstyrsNiveau;
+  private int stålpris;
+  private int regAfgift;
+  private int co2Udledning;
+  private boolean udlejet;
+  private String udlejningsdato;
+  private boolean erSD;
+
+}
---
+++ src/main/java/com/example/biludlejning/repository/BilRepository.java
@@ -0,0 +1,8 @@
+package com.example.biludlejning.repository;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BilRepository {
+}
