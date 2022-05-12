0f8bc09c90251b2b51c60ec26fb2cf86d47aacc0
FrederikTC <fred47i4@stud.kea.dk>
2022-05-12 12:00:30 +0200

    css + frontend v.7

--- src/main/java/com/example/biludlejning/controller/HomeController.java
+++ src/main/java/com/example/biludlejning/controller/HomeController.java
@@ -17,105 +17,107 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class HomeController {
 
-  BrugerService brugerservice;
-  KundeOgLejeaftaleService kundeOgLejeaftaleService;
-  String fejlmeddelse = "";
-  BilService bilservice;
+    BrugerService brugerservice;
+    KundeOgLejeaftaleService kundeOgLejeaftaleService;
+    String fejlmeddelse = "";
+    BilService bilservice;
 
-  public HomeController(BrugerService brugerservice, KundeOgLejeaftaleService kundeOgLejeaftaleService,  BilService bilservice) {
-    this.brugerservice = brugerservice;
-    this.kundeOgLejeaftaleService = kundeOgLejeaftaleService;
-    this.bilservice = bilservice;
-  }
-
-  @GetMapping("/")
-  public String index(HttpSession httpSession, Model model) {
-    model.addAttribute("brugerRolle", httpSession.getAttribute("brugerRolle"));
-    model.addAttribute("brugere",brugerservice.seBrugere());
-    return "index";
-  }
-  @GetMapping("/sletbrugere")
-  public String sletbrugere(HttpSession httpSession, Model model) {
-    model.addAttribute("brugerRolle", httpSession.getAttribute("brugerRolle"));
-    model.addAttribute("brugere",brugerservice.seBrugere());
-    return "/sletbrugere";
-  }
-
-@GetMapping("/slet/{brugernavn}")
-public String sletBruger(@PathVariable("brugernavn") String brugernavn){
-    brugerservice.sletBruger(brugernavn);
-  return "redirect:/";
-}
-
-  @GetMapping("/opretbruger")
-  public String visBrugerOprettelse() {
-    return "opretbruger";
-  }
-
-  @PostMapping("/opretbruger")
-  public String opretBruger(@RequestParam String brugernavn, @RequestParam String rolle, @RequestParam String kodeord) {
-    brugerservice.opretBruger(new Bruger(brugernavn, rolle, kodeord));
-    return "redirect:/login";
-  }
-
-  @GetMapping("/login")
-  public String visLogin(HttpSession httpSession) {
-    httpSession.getAttribute("fejlmeddelse");
-    return "login";
-  }
-
-  @GetMapping("/logud")
-  public String logud(HttpSession httpSession){
-    httpSession.setAttribute("brugerRolle",null);
-    return "redirect:/";
-  }
-
-  @PostMapping("/login")
-  public String login(@RequestParam String brugernavn, @RequestParam String kodeord,
-                      HttpSession httpSession) {
-    String returnStatement = "redirect:/";
-    if (brugernavn.equalsIgnoreCase("admin") && kodeord.equalsIgnoreCase("admin")) {
-      Bruger admin = new Bruger(brugernavn,"admin", kodeord);
-      httpSession.setAttribute("brugerRolle", admin.getRolle());
-      httpSession.setAttribute("bruger", admin);
-    } else {
-      Bruger bruger = brugerservice.findBruger(brugernavn);
-      //Tjekker om brugernavn og kodeord hører sammen, derefter smider den brugeren ind på indexsiden til den tilhørende rolle.
-      //Session bliver også oprettet med brugeren som logger på.
-      if (brugerservice.korrektLogin(brugernavn, kodeord, bruger)) {
-        httpSession.setAttribute("brugerRolle", bruger.getRolle());
-        httpSession.setAttribute("bruger", bruger);
-      } else returnStatement = "redirect:/login";
-      //Hvis brugeren ikke findes, bliver der redirected, og her kan vi tilgå en model attribute "fejlmeddelse" der viser hvorfor.
-      httpSession.setAttribute("fejlmeddelse", fejlmeddelse = brugerservice.loginFejl(bruger, kodeord));
+    public HomeController(BrugerService brugerservice, KundeOgLejeaftaleService kundeOgLejeaftaleService, BilService bilservice) {
+        this.brugerservice = brugerservice;
+        this.kundeOgLejeaftaleService = kundeOgLejeaftaleService;
+        this.bilservice = bilservice;
     }
-    return returnStatement;
-  }
 
-  @GetMapping("/unlimitedBiltyper")
-  public String unlimitedBiltyper(Model model) {
-    model.addAttribute("biler", bilservice.seBiler());
-    model.addAttribute("modelAntal", bilservice.modelAntal());
-    model.addAttribute("enAfHverModel", bilservice.enAfHverModel());
-    return "unlimitedBiltyper";
-  }
-  @GetMapping("/limitedBiltyper")
-  public String limitedBiltyper() {
-    return "limitedBiltyper";
-  }
+    @GetMapping("/")
+    public String index(HttpSession httpSession, Model model) {
+        model.addAttribute("brugerRolle", httpSession.getAttribute("brugerRolle"));
+        model.addAttribute("brugere", brugerservice.seBrugere());
+        return "index";
+    }
 
-  @GetMapping("/opretkunde")
-  public String opretKunde(){
-    return "opretkunde";
-  }
+    @GetMapping("/sletbrugere")
+    public String sletbrugere(HttpSession httpSession, Model model) {
+        model.addAttribute("brugerRolle", httpSession.getAttribute("brugerRolle"));
+        model.addAttribute("brugere", brugerservice.seBrugere());
+        return "/sletbrugere";
+    }
 
-  @GetMapping("/opretlejeaftale")
-  public String opretLejeaftale(@RequestParam int kundeid, @RequestParam int vognnummer, @RequestParam double forskudsbetaling,
-                                 @RequestParam double månedligbetaling, @RequestParam int antalbetalinger, Model model){
-    LejeAftale lejeAftale = new LejeAftale(kundeid,vognnummer,forskudsbetaling,månedligbetaling,antalbetalinger);
-    kundeOgLejeaftaleService.lavLejeKontrakt(lejeAftale);
-    model.addAttribute("lejekontrakt",kundeOgLejeaftaleService.lavLejeKontrakt(lejeAftale));
-    return "opretlejeaftale";
-  }
+    @GetMapping("/slet/{brugernavn}")
+    public String sletBruger(@PathVariable("brugernavn") String brugernavn) {
+        brugerservice.sletBruger(brugernavn);
+        return "redirect:/";
+    }
+
+    @GetMapping("/opretbruger")
+    public String visBrugerOprettelse() {
+        return "opretbruger";
+    }
+
+    @PostMapping("/opretbruger")
+    public String opretBruger(@RequestParam String brugernavn, @RequestParam String rolle, @RequestParam String kodeord) {
+        brugerservice.opretBruger(new Bruger(brugernavn, rolle, kodeord));
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String visLogin(HttpSession httpSession) {
+        httpSession.getAttribute("fejlmeddelse");
+        return "login";
+    }
+
+    @GetMapping("/logud")
+    public String logud(HttpSession httpSession) {
+        httpSession.setAttribute("brugerRolle", null);
+        return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String brugernavn, @RequestParam String kodeord,
+                        HttpSession httpSession) {
+        String returnStatement = "redirect:/";
+        if (brugernavn.equalsIgnoreCase("admin") && kodeord.equalsIgnoreCase("admin")) {
+            Bruger admin = new Bruger(brugernavn, "admin", kodeord);
+            httpSession.setAttribute("brugerRolle", admin.getRolle());
+            httpSession.setAttribute("bruger", admin);
+        } else {
+            Bruger bruger = brugerservice.findBruger(brugernavn);
+            //Tjekker om brugernavn og kodeord hører sammen, derefter smider den brugeren ind på indexsiden til den tilhørende rolle.
+            //Session bliver også oprettet med brugeren som logger på.
+            if (brugerservice.korrektLogin(brugernavn, kodeord, bruger)) {
+                httpSession.setAttribute("brugerRolle", bruger.getRolle());
+                httpSession.setAttribute("bruger", bruger);
+            } else returnStatement = "redirect:/login";
+            //Hvis brugeren ikke findes, bliver der redirected, og her kan vi tilgå en model attribute "fejlmeddelse" der viser hvorfor.
+            httpSession.setAttribute("fejlmeddelse", fejlmeddelse = brugerservice.loginFejl(bruger, kodeord));
+        }
+        return returnStatement;
+    }
+
+    @GetMapping("/unlimitedBiltyper")
+    public String unlimitedBiltyper(Model model) {
+        model.addAttribute("biler", bilservice.seBiler());
+        model.addAttribute("modelAntal", bilservice.modelAntal());
+        model.addAttribute("enAfHverModel", bilservice.enAfHverModel());
+        return "unlimitedBiltyper";
+    }
+
+    @GetMapping("/limitedBiltyper")
+    public String limitedBiltyper() {
+        return "limitedBiltyper";
+    }
+
+    @GetMapping("/opretkunde")
+    public String opretKunde() {
+        return "opretkunde";
+    }
+
+    @GetMapping("/opretlejeaftale")
+    public String opretLejeaftale(@RequestParam int kundeid, @RequestParam int vognnummer, @RequestParam double forskudsbetaling,
+                                  @RequestParam double månedligbetaling, @RequestParam int antalbetalinger, Model model) {
+        LejeAftale lejeAftale = new LejeAftale(kundeid, vognnummer, forskudsbetaling, månedligbetaling, antalbetalinger);
+        kundeOgLejeaftaleService.lavLejeKontrakt(lejeAftale);
+        model.addAttribute("lejekontrakt", kundeOgLejeaftaleService.lavLejeKontrakt(lejeAftale));
+        return "opretlejeaftale";
+    }
 
 }
